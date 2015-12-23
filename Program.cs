d32c47df3a20848da764b05db6eb35ff2f69b91b
warpech <warpech@gmail.com>
2015-12-23 01:41:28 +0100

    fix async code calls (https://github.com/Starcounter/Starcounter/issues/3210)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Starcounter;
 using Octokit;
+using Starcounter.Internal;
 
 namespace GitHubImporter {
     class Program {
@@ -11,6 +12,9 @@ namespace GitHubImporter {
         }
 
         static async void RefreshGitHubData() {
+            var schedulerId = StarcounterEnvironment.CurrentSchedulerId;
+            var appName = StarcounterEnvironment.AppName;
+
             try {
                 Console.WriteLine("Connecting to GH...");
                 var github = new GitHubClient(new ProductHeaderValue("GitHubImporter"));
@@ -18,18 +22,26 @@ namespace GitHubImporter {
                 Console.WriteLine("Getting user info");
                 var user = await github.User.Get("warpech");
 
-                Console.WriteLine("Saving to db"); //does not show in Administrator
-                Db.Transact(() => {
-                    Console.WriteLine("Creating new User"); //does not execute at all (or breakpoint does not stop here)
-                    new User() {
-                        Name = user.Name,
-                        Url = user.HtmlUrl,
-                        AvatarUrl = user.AvatarUrl
-                    };
-                }); 
+                new DbSession().RunAsync(() => {
+                    StarcounterEnvironment.RunWithinApplication(appName, () => {
+                        Console.WriteLine("Saving to db");
+                        Db.Transact(() => {
+                            Console.WriteLine("Creating new User");
+                            new User() {
+                                Name = user.Name,
+                                Url = user.HtmlUrl,
+                                AvatarUrl = user.AvatarUrl
+                            };
+                        });
+                    });
+                }, schedulerId);
             }
             catch (Exception ex) {
-                Console.WriteLine("Exception caught: " + ex); //does not show in Administrator
+                new DbSession().RunAsync(() => {
+                    StarcounterEnvironment.RunWithinApplication(appName, () => {
+                        Console.WriteLine("Exception caught: " + ex); //does not show in Administrator
+                    });
+                }, schedulerId);
             }
         }
     }
